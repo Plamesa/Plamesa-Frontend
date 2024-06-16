d8b7d2e6470869beb037a583b81b883083b6ed8a
Ivan <ivitacoronte@hotmail.com>
2024-06-16 20:17:03 +0100

    dieta

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -18,9 +18,8 @@ export enum Allergen {
 
 // Definición del enum IncompatibilidadAlimenticia
 export enum Diet {
-  Diabeticos = "No adecuado para diabeticos", // (alto contenido de azúcares o carbohidratos)
-  Vegetarianos = "No adecuado para vegetarianos", // (contiene carne o pescado)
-  Veganos = "No adecuado para veganos", // (contiene ingredientes de origen animal)
+  Vegetarianos = "Vegetarianos", // (contiene carne o pescado)
+  Veganos = "Veganos", // (contiene ingredientes de origen animal)
   Vacio = ""
 }
 
@@ -60,6 +59,7 @@ export enum FoodGroup {
 export enum Units {
   gr = "gr",
   l = "l",
+  ud = "ud",
 }
 
 export enum NutrientsTypes {
